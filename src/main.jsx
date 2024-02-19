90001c268a5267a96e9464cd0b6eb66de339c966
Joao Pedro <joaopedrolamim@yahoo.com.br>
2024-02-19 14:26:44 -0300

    Commit initial

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Details } from "./pages/Details"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Details />
+  </React.StrictMode>
+)
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+
+export function Details() {
+  return (
+    <>
+      <h1>Hello Word!</h1>
+      <span>João Pedro</span>
+    </>
+  )
+}
